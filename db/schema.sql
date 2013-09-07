8b220338712d0130213ac41d6519353e7bb9f909
Arthur Burkart <aburkart@wesleyan.edu>
2013-09-07 00:31:07 -0400

    More changes to database schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,18 +4,18 @@ location TEXT,
 program INTEGER,
 quiet INTEGER,
 restriction INTEGER,
-capacity INTEGER,
 description TEXT,
 image TEXT,
 geo_x REAL,
 geo_y REAL,
-wood INTEGER
+wood INTEGER, 
+units INTEGER
 )
 
 CREATE TABLE room (
 _uid INTEGER PRIMARY KEY,
 FOREIGN KEY(residence_id) REFERENCES residence(_uid),
-room_number TEXT,
+room_number INTEGER,
 size INTEGER
 )
 
@@ -37,5 +37,6 @@ CREATE TABLE selection (
 _uid INTEGER PRIMARY KEY,
 FOREIGN KEY(group_id) REFERENCES group(_uid),
 FOREIGN KEY(room_id) REFERENCES group(_uid),
+FOREIGN KEY(student_id) REFERENCES group(_uid),
 priority INTEGER
 )
